f16da1bc30982adc97d02bdc89c0a8fd6cb42789
alexhutchings2004 <alexhutchings04@gmail.com>
2025-03-26 16:33:42 +0000

    Fix module errors

--- config-overrides.js
+++ config-overrides.js
@@ -8,7 +8,13 @@ module.exports = function override(config, env) {
     stream: require.resolve('stream-browserify'),
     buffer: require.resolve('buffer/'),
     util: require.resolve('util/'),
-    process: require.resolve('process/browser'),
+    process: require.resolve('process/browser.js'), // Ensure .js extension is specified
+  };
+
+  // Add alias for process/browser.js
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    'process/browser': require.resolve('process/browser.js'),
   };
 
   // Add process and buffer as plugins
